d1251a49170c4a37585bd85a3ca9929c183ee2c1
Diego Melgoza <drmelgoza@icloud.com>
2026-04-22 14:20:18 -0700

    add data type parsing

--- InduceC45.py
+++ InduceC45.py
@@ -4,12 +4,27 @@
 # decision tree in JSON format.
 # Essentially, you can view the InduceC45.py program as a wrapper around the .fit() and .save_tree()
 # methods of the c45 class
+from scrapy.spiders.sitemap import regex
+
 from c45 import C45
 import pandas as pd
 
-C45()
+tree = C45()
 def read_csv(test_csv):
-    # read csv
+    # read csv, including 1st 3
     df = pd.read_csv(test_csv)
+    #isolate data types and class var y
+    #array
+    col_names = df.columns()
+    data_types = df.iloc[0]
+    class_var = df.iloc[1][0]
+    df = df.drop([0, 1])
+    #array
+    data_types = data_types.replace({"0":"float64", r"^[1-9]$":"category"}, regex=True)
+    col_types = pd.Series(data_types, index=col_names)
+    #separate class var y and X
+    y = df.loc[:, class_var]
+    X = df.drop(columns=class_var)
+
 
     pass
