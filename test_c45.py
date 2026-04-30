c0b3be7361c8dd2d39eac24a3b8ddcc40338122b
Diego Melgoza <drmelgoza@icloud.com>
2026-04-30 01:23:45 -0700

    fix test_c45.py spacing

--- test_c45.py
+++ test_c45.py
@@ -1,7 +1,6 @@
 import unittest
 from c45 import C45Tree
 
-
 class MyTestCase(unittest.TestCase):
     def test_c45_fit(self):
         my_tree = C45Tree()
@@ -9,8 +8,6 @@ class MyTestCase(unittest.TestCase):
 
     def test_induce_c45(self):
         my_tree = C45Tree()
-
-
 
 
 if __name__ == '__main__':
